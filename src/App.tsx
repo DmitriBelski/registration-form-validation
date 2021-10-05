c15b40099e2f8cff2f1e73758437f7c05e4ab9a3
DmitriBelski <51079926+DmitriBelski@users.noreply.github.com>
2021-10-05 22:08:18 +0300

    Rename .js to .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-import './App.css';
+import 'App.css'
 
-function App() {
+function App () {
   return (
     <div>
       <h1>Amigo Registration</h1>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
