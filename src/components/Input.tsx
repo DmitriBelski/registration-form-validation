5dfbd495be857c6296450e423e379931afdb71ee
DmitriBelski <51079926+DmitriBelski@users.noreply.github.com>
2021-10-12 22:49:58 +0300

    Use class component for Input & RegistrationForm

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,31 +2,36 @@ import * as React from 'react'
 import './Input.scss'
 
 interface InputProps {
-  htmlFor?: string;
+  name?: string;
   label?: string;
   inputType: string;
   placeholder?: string;
   validateMessage?: string;
-  requiredMessage?: string;
+  value?: string;
+  onInputChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   children?: React.ReactNode;
 }
 
-const Input: React.FC<InputProps> = (props) => {
-  return (
-    <label className="input-group form__group" htmlFor={props.htmlFor}>
-      <span className="input-group__label text-label-regular">{props.label}</span>
-      <input
-        className="input-group__input input text-input"
-        type={props.inputType}
-        id={props.htmlFor}
-        placeholder={props.placeholder}
-      />
-      <span className="input-group__message text-danger" hidden>
-        {props.validateMessage}
-      </span>
-      {props.children}
-    </label>
-  )
+class Input extends React.Component<InputProps> {
+  render() {
+    return (
+      <label className="input-group form__group">
+        <span className="input-group__label text-label-regular">{this.props.label}</span>
+        <input
+          className="input-group__input input text-input"
+          name={this.props.name}
+          type={this.props.inputType}
+          placeholder={this.props.placeholder}
+          value={this.props.value}
+          onChange={this.props.onInputChange}
+        />
+        <span className="input-group__message text-danger" hidden={!this.props.validateMessage}>
+          {this.props.validateMessage}
+        </span>
+        {this.props.children}
+      </label>
+    )
+  }
 }
 
 export { Input }
--- src/views/RegistrationForm.tsx
+++ src/views/RegistrationForm.tsx
@@ -1,57 +1,85 @@
 import * as React from 'react'
 import './RegistrationForm.scss'
-import { Input } from 'components/Input'
 import { Select } from 'components/Select'
 import { CheckBox } from 'components/CheckBox'
+import { Input } from 'components/Input'
 
-function RegistrationForm() {
-  return (
-    <form className="form">
-      <div className="header form__header">
-        <h1 className="header__title text-title-regular">Регистрация</h1>
-        <span className="header__subtitle text-subtitle-regular">
-          Уже есть аккаунт?
-          <a className="header__subtitle-link link text-subtitle-accent" href="/" target="_blank">
-            Войти
-          </a>
-        </span>
-      </div>
-      <Input
-        htmlFor="name"
-        label="Имя"
-        inputType="text"
-        placeholder="Введите ваше имя"
+interface RegistrationFormState {
+  name: string,
+  email: string,
+  phone: string,
+  language: string,
+  agreement: boolean,
+}
+
+class RegistrationForm extends React.Component<{}, RegistrationFormState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      name: '',
+      email: '',
+      phone: '',
+      language: '',
+      agreement: false
+    }
+  }
+
+  render() {
+    return (
+      <form className="form">
+        <div className="header form__header">
+          <h1 className="header__title text-title-regular">Регистрация</h1>
+          <span className="header__subtitle text-subtitle-regular">
+            Уже есть аккаунт?
+            <a className="header__subtitle-link link text-subtitle-accent" href="/" target="_blank">
+              Войти
+            </a>
+          </span>
+        </div>
+        <Input
+          name="name"
+          label="Имя"
+          inputType="text"
+          placeholder="Введите ваше имя"
         validateMessage="Введено не корректное значение"
-      />
-      <Input
-        htmlFor="email"
-        label="Email"
-        inputType="email"
-        placeholder="Введите ваш email"
+          onInputChange={(event) => this.setState({ name: event.target.value })}
+          value={this.state.name}
+        />
+        <Input
+          name="email"
+          label="Email"
+          inputType="email"
+          placeholder="Введите ваш email"
         validateMessage="Введено не корректное значение"
-      />
-      <Input
-        htmlFor="cell"
-        label="Номер телефона"
-        inputType="tel"
-        placeholder="Введите номер телефона"
+          onInputChange={(event) => this.setState({ email: event.target.value })}
+          value={this.state.email}
+        />
+        <Input
+          name="phone"
+          label="Номер телефона"
+          inputType="tel"
+          placeholder="Введите номер телефона"
         validateMessage="Введено не корректное значение"
-      />
-      <Select
-        label="Язык"
-        inputType="text"
-        placeholder="Язык"
-        validateMessage="Выберите язык"
-      >
-      </Select>
-      <CheckBox>
-        Принимаю&nbsp;
-        <a className="link text-label-accent" href="/" target="_blank">условия</a>
-        &nbsp;использования
-      </CheckBox>
+          onInputChange={(event) => this.setState({ phone: String(event.target.value) })}
+          value={this.state.phone}
+        />
+        <Select
+          name="language"
+          label="Язык"
+          inputType="text"
+          placeholder="Язык"
+          validateMessage="Выберите язык"
+        >
+        </Select>
+        <CheckBox>
+          Принимаю&nbsp;
+          <a className="link text-label-accent" href="/" target="_blank">условия</a>
+          &nbsp;использования
+        </CheckBox>
       <button className="button button--disabled text-button-regular text-button-disabled" type="submit">Зарегистрироваться</button>
-    </form>
-  )
+      </form>
+    )
+  }
 }
 
 export { RegistrationForm }
